4b882d0f50d9cac34a6c1ab148a495ac8c534c15
Mickael <85728035+MickaelGP@users.noreply.github.com>
2024-05-24 15:51:39 +0200

    Add update delete for api

--- app/Http/Controllers/ProductApiController.php
+++ app/Http/Controllers/ProductApiController.php
@@ -4,14 +4,86 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductApiResource;
 use App\Models\ProductApi;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProductApiController extends Controller
 {
-    public function showProduct()
+    /**
+     *Display a listing of all products
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function index()
     {
+        return ProductApiResource::collection(ProductApi::all());
+    }
 
+    /**
+     * Store a newly created product in storage
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): \Illuminate\Http\JsonResponse
+    {
+        //Validate the incoming request data
+        $data = $request->validate([
+            'code' => ['required', 'string', 'max:60', 'unique:products,code'],
+            'product_name' => ['required', 'string', 'max:150'],
+            'categories' => ['required', 'string', 'max:60'],
+            'brands' => ['required', 'string', 'max:60'],
+            'image_url' => ['string', 'nullable', 'max:255'],
+        ]);
+        //Check if result of $data exist in database
+        if (!ProductApi::where('code', $data['code'])->exists()) {
+            //Add product in database
+            $product = ProductApi::create($data);
+            return response()->json(new ProductApiResource($product), 200);
+        } else {
+            //Return message Product already exists
+            return response()->json([
+                'message' => "Product already exists",
+            ], 400);
+        }
+    }
 
-        return ProductApiResource::collection(ProductApi::limit(10)->get());
+    /**
+     * Update product by Id
+     * @param ProductApi $productApi
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function update(Request $request, ProductApi $productApi): \Illuminate\Http\JsonResponse
+    {
+        $data = $request->validate([
+            'code' => ['required', 'string', 'max:60'],
+            'product_name' => ['required', 'string', 'max:150'],
+            'categories' => ['required', 'string', 'max:60'],
+            'brands' => ['required', 'string', 'max:60'],
+            'image_url' => ['string', 'nullable', 'max:255'],
+        ]);
+        if (!ProductApi::find($productApi)) {
+            return response()->json([
+                'message' => "Product not found",
+            ], 400);
+        } else {
+            $productApi->update($data);
+            return response()->json(new ProductApiResource($productApi), 200);
+        }
+    }
+
+    /**
+     * Delete product by Id
+     *
+     * @param ProductApi $productApi
+     * @return JsonResponse
+     */
+    public function destroy(ProductApi $productApi): \Illuminate\Http\JsonResponse
+    {
+        $productApi->delete();
+
+        return response()->json(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,7 @@ use Illuminate\Support\Facades\Route;
   //  return $request->user();
 //})->middleware('auth:sanctum');
 
-Route::get('products', [\App\Http\Controllers\ProductApiController::class, 'showProduct']);
-
+Route::get('products', [\App\Http\Controllers\ProductApiController::class, 'index']);
+Route::post('products', [\App\Http\Controllers\ProductApiController::class, 'store']);
+Route::patch('products/{productApi}', [\App\Http\Controllers\ProductApiController::class, 'update']);
+Route::delete('products/{productApi}', [\App\Http\Controllers\ProductApiController::class, 'destroy']);
